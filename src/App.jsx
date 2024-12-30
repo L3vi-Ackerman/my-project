e3b658d7888dd878849d2f26d3e038c1d543fac9
L3vi Ackerman <l3vi3211@gmail.com>
2024-12-30 14:18:52 +0545

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Accordian from './Components/Accordian'
+const App = () => {
+  return (
+    <div className="App">
+      <Accordian />
+    </div>
+  )
+}
+
+export default App
